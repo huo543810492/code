c46e94a25f8b1b8e01b9c1408276c50251cb871d
phoenix <phoenixtec6@gmail.com>
2024-07-09 11:59:08 +0800

    update

--- fighting/src/main/java/Interview/interviewAlgorithms/MaxContiguousSumNoLargerThanK.java
+++ fighting/src/main/java/Interview/interviewAlgorithms/MaxContiguousSumNoLargerThanK.java
@@ -6,45 +6,35 @@ import java.util.List;
 
 public class MaxContiguousSumNoLargerThanK {
     static List<List<Integer>> res = new ArrayList<>();
-    public static long getMaxProfit(List<Integer> pnl, int k) {
-        // Write your code here
-        int[] memo = new int[k+1];//store prefix sum
-        if(pnl.size()==0)
-        {
+    public static long getMaxProfit(List<Integer> arr, int k) {
+        if(arr==null || arr.size()==0){
             return 0;
         }
-        int res=pnl.get(0);
-        for(int i=1;i<=k;i++)
-        {
-            res = Math.max(slidingWindow(pnl,i, memo),res);
+        int res=arr.get(0);
+        for(int i=1;i<arr.size();i++){
+            res=Math.max(slidingWindow(arr,i),res);
         }
         return res;
     }
 
-    public static int slidingWindow(List<Integer> list, int size, int[] memo){
-        int res=list.get(0);
-        int sum=0;
-        int left=0;
-        for(int i=size-1;i<list.size();i++)
-        {
-            if(i<size){
-                sum=memo[size-1]+list.get(i);
-                memo[size]=sum;
-            }
-            else if(i==size){
-                res=sum;
-            }
-            else if(i>size){
+    public static int slidingWindow(List<Integer> list, int size){
+        int[] memo = new int[list.size()];
+        int sum=0,count=0,left=0,res=0;
+        for (int i=0;i<list.size();i++){
+            sum+=list.get(i);
+            count++;
+            if(count>size){
                 sum-=list.get(left);
-                sum+=list.get(i);
+                count--;
                 left++;
-                res=Math.max(res,sum);
             }
+            res=Math.max(res,sum);
         }
         return res;
     }
 
     public static void main(String[] args) {
+        //16
         System.out.println(getMaxProfit(Arrays.asList(8,2,5,-7,8,-6,4,1,-9,5),5));
     }
 }
